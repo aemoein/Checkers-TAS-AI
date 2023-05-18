4e8abd7b7805382bec40617115d385c5d4f0749c
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-05-18 23:45:04 +0300

    Game FIles
    Added game file which includes the main logic of the checkers game and how its played and start of the main file

---
+++ checkers/game.py
@@ -0,0 +1,40 @@
+import pygame
+from .constants import RED, WHITE, BLUE, SQUARE_SIZE
+from checkers.board import Board
+
+class Game:
+    def __init__(self, win):
+        self._init()
+        self.win = win
+
+    def update(self):
+        self.board.draw(self.win)
+        self.draw_valid_moves(self.valid_moves)
+        pygame.display.update()
+        
+    def _init(self):
+        self.selected = None
+        self.board = Board()
+        self.turn = RED
+        self.valid_moves = {}
+
+    def winner(self):
+        return self.board.winner()
+    
+    def reset(self):
+        self._init()
+
+    def select(self, row, col):
+        if self.selected:
+            result = self._move(row, col)
+            if not result:
+                self.selected = None
+                self.select(row, col)
+        
+        piece = self.board.get_piece(row, col)
+        if piece != 0 and piece.color == self.turn:
+            self.selected = piece
+            self.valid_moves = self.board.get_valid_moves(piece)
+            return True
+            
+        return False
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import pygame
+from checkers.constants import WIDTH,HEIGHT,SQUARE_SIZE
+FPS = 60.0
+
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Checkers!!')
+
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
