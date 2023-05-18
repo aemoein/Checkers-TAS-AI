05be9bf79d7beeb96a5d2041dc355dc124caffcd
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-05-19 00:45:49 +0300

    Game file complated
    the complete game file as well as the main first run case complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from checkers.constants import WIDTH,HEIGHT,SQUARE_SIZE
+from checkers.board import Board
+from checkers.game import Game
+
 FPS = 60.0
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -11,3 +14,24 @@ def get_row_col_from_mouse(pos):
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE
     return row, col
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+    game = Game(WIN)
+
+    while run:
+       # clock.tick()
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+    
+        board.draw(WIN)
+        pygame.display.update()
+    pygame.quit()
+       
